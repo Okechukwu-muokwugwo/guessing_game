bfd1661343d61e994b0ccbeedea1df3dd6869ef5
Okechukwu Muokwugwo <okei.muokwugwo@gmail.com>
2023-08-25 13:26:14 +0100

    Implement Guess struct

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,23 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value should be between 1 and 100, got {}.", value);
+        }
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
 
